ddbbea68eb8fafed313e93f066fff8dd8f44f5ba
John Cherba <john.cherba@mchsi.com>
2025-03-05 00:14:40 +0000

    Remove unused useMemo declaration

--- src/components/CaptureTheFlag/index.js
+++ src/components/CaptureTheFlag/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import useTypewriter from "../../hooks/useTypewriter";
 
 function CaptureTheFlag() {
